59be40536bdb87d135084e6aed4009456de4e2bf
Osqui11 <calito200051@gmail.com>
2021-03-10 13:19:44 -0600

    practica

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 
 const server = Server.instance;
-
+//rama de oscar
 server.app.use(bodyParser.urlencoded({ extended: true}));
 server.app.use(bodyParser.json());
 
